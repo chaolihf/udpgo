041c959905f50300d535930adb5065db393ddff6
chaolihf <chaolihf@chinateleco.com>
2022-11-01 21:25:51 +0800

    modify test

--- kafka/kafkaClient_test.go
+++ kafka/kafkaClient_test.go
@@ -3,6 +3,6 @@ package kafka
 import "testing"
 
 func TestSending(t *testing.T) {
-	p := initKafka([]string{"127.0.0.1:8450"})
+	p := InitKafka([]string{"127.0.0.1:8450"})
 	p.SendMesssage([]byte(""), "aa", "key")
 }
