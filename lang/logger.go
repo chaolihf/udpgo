480b03cecaa073562b76f2531452992c4186616e
chaolihf <chaolihf@chinateleco.com>
2022-11-03 18:07:16 +0800

    add left string

--- lang/logger.go
+++ lang/logger.go
@@ -7,3 +7,16 @@ func InitLogger() *zap.Logger {
 	defer logger.Sync()
 	return logger
 }
+
+//@title return left string ,support utf-8 and without panic when outbound
+func LeftString(data string, count int) string {
+	if len(data) <= count {
+		return data
+	}
+	utfData := []rune(data)
+	if len(utfData) <= count {
+		return data
+	} else {
+		return string(utfData[0:count])
+	}
+}
--- lang/logger_test.go
+++ lang/logger_test.go
@@ -6,3 +6,31 @@ func TestLogger(t *testing.T) {
 	logger := InitLogger()
 	logger.Info("aa")
 }
+
+func TestLeft(t *testing.T) {
+	var result string
+	result = LeftString("abc", 1)
+	if "a" != result {
+		t.Fatal("fail 1")
+	}
+	result = LeftString("abc", 3)
+	if "abc" != result {
+		t.Fatal("fail 2")
+	}
+	result = LeftString("abc", 4)
+	if "abc" != result {
+		t.Fatal("fail 3")
+	}
+	result = LeftString("中ab吧c", 1)
+	if "中" != result {
+		t.Fatal("fail 4")
+	}
+	result = LeftString("a中b吧c", 3)
+	if "a中b" != result {
+		t.Fatal("fail 5")
+	}
+	result = LeftString("a中b吧c", 40)
+	if "a中b吧c" != result {
+		t.Fatal("fail 6")
+	}
+}
